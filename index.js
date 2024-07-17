975bbf46957c4ca238b1ec4cf9bbe9cd89453bd8
Practical Web Dev <info.practicalwebdev@gmail.com>
2024-07-16 18:29:44 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const menuBtn = document.querySelector(".mobile-menu-btn");
+
+menuBtn.addEventListener("click", function () {
+
+const mainNav = document.querySelector(".main-nav")
+mainNav.classList.toggle("show")
+
+
+})
